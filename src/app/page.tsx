4a34cf72d0fbddddc060442d54787c295b0ec2c4
Lucas Paiva <lcspaiva87@gmail.com>
2025-07-04 21:54:42 -0300

    feat: adiciona novo arquivo SVG de recurso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,50 @@
 import Footer from "@/components/footer/footer";
-
 import { Header } from "./_components/header";
 import { CardsCustom } from "@/components/cards-custom";
+import home from "../../public/assets/feature.svg"
+
 
 export default function Home() {
-  const cards = [
-    {
-      title: "Crie um catálogo de produtos online em poucos minutos",
-      badge: "simples",
-    },
-    {
-      title: "Venda para seu público através de uma plataforma única",
-      badge: "prático",
-    },
-    {
-      title: "Tenha uma loja online personalizada com a cara da sua marca",
-      badge: "personalizável",
-    },
-  ]
+  const cards: {
+    title: string;
+    badge: string;
+    align: "flex-col" | "flex-row";
+    image?: string;
+    button?: string;
+  }[] = [
+      {
+        title: "Crie um catálogo de produtos online em poucos minutos",
+        badge: "simples",
+        align: "flex-col",
+      },
+      {
+        title: "Venda para seu público através de uma plataforma única",
+        badge: "prático",
+        align: "flex-col",
+
+      },
+      {
+        title: "Tenha uma loja online personalizada com a cara da sua marca",
+        badge: "personalizável",
+        align: "flex-col",
+        button: "Criar loja grátis",
+        image: home.src,
+      },
+    ]
   return (
     <div className="flex flex-col gap-20">
       <Header />
-      <div className="grid grid-cols-2 gap-6 px-52">
-        {cards.map((card) => (
-          <CardsCustom key={card.title} title={card.title} badge={card.badge} />
+      <div className="grid grid-cols-2 gap-6 px-52 w-full">
+        {cards.map((card, idx) => (
+          <CardsCustom
+            key={card.title}
+            title={card.title}
+            badge={card.badge}
+            align={card.align}
+            image={card.image}
+            button={card.button}
+            className={idx === 2 ? "col-span-2 flex flex-row gap-2 justify-between" : ""}
+          />
         ))}
       </div>
 
--- src/components/cards-custom.tsx
+++ src/components/cards-custom.tsx
@@ -1,20 +1,41 @@
+import { ArrowRightIcon } from "lucide-react";
 import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 import { Card } from "./ui/card"
 
 interface CardsCustomProps {
   title: string;
   badge: string;
-
+  align: "flex-row" | "flex-col";
+  image?: string;
+  button?: string;
+  className?: string;
 }
 
-export function CardsCustom({ title, badge }: CardsCustomProps) {
+export function CardsCustom({ title, badge, align, image, button, className }: CardsCustomProps) {
   return (
-    <Card className="w-full h-full p-12 bg-gray-500 border-none">
-      <div className="flex flex-col  gap-2">
-        <Badge variant="outline" className="bg-blue-400 text-blue-200 border-none ">{badge}</Badge>
-        <h2 className="font-bold heading-lg text-white">{title}</h2>
+    <Card className={` h-full p-12 bg-gray-500 border-none ${className ?? ""}`}>
+      <div className={`flex ${align} gap-32 w-80`}>
+        <div className=" gap-4 flex flex-col">
+          <Badge variant="outline" className="bg-blue-400 text-blue-200 border-none ">{badge}</Badge>
+          <h2 className="font-bold heading-lg text-white">{title}</h2>
+        </div>
+        {button && (
+          <Button className="flex items-center justify-center">
+            {button}
+            <ArrowRightIcon className="w-4 h-4 ml-2" />
+          </Button>
+        )}
       </div>
+      {image && (
+        <div className="flex flex-row  items-stretch justify-center">
+          <div className="flex flex-col justify-between">
+            <img src={image} alt="imagem 1" width={440} height={327} />
 
+          </div>
+
+        </div>
+      )}
     </Card >
   )
 }
